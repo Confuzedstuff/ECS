1d1d3e3dd929a1a06e56ddfcdd04149be9b1a70b
willem.pelser <willem.pelser@entelect.co.za>
2023-11-28 18:51:22 +0200

    Refactor EntityIndex

--- ECSDomain/EntityIndex.cs
+++ ECSDomain/EntityIndex.cs
@@ -1,22 +1,10 @@
-﻿using System;
-
-namespace ECSDomain;
-public readonly struct EntityIndex : IEquatable<EntityIndex>
+﻿namespace ECSDomain;
+public readonly struct EntityIndex(in int index, in int generation) : IEquatable<EntityIndex>
 {
+    public readonly int Index = index;
+    public readonly int Generation = generation;
     public bool Equals(EntityIndex other) => Index == other.Index && Generation == other.Generation;
-
     public override bool Equals(object? obj) => obj is EntityIndex other && Equals(other);
-
     public override int GetHashCode() => HashCode.Combine(Index, Generation);
-
-    public readonly int Index;
-    public readonly int Generation;
-
-    public EntityIndex(in int index, in int generation)
-    {
-        Index = index;
-        Generation = generation;
-    }
-
     public override string ToString() => $"{Index}:{Generation}";
 }
